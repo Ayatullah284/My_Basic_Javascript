8f9b9572ca0eed5fcfc9e1d5d909e8dc5876b9e0
Ayatullah284 <ayatullah284@gmail.com>
2022-12-13 18:01:31 +0600

    motamti 4th mileston ses

---
+++ HW.js
@@ -0,0 +1,38 @@
+// grade point 
+function gradePoint(number) {
+    if (number >= 80) {
+        console.log('A +');
+    }
+    else if (number >= 75) {
+        console.log('A -');
+    }
+    else if (number >= 75) {
+        console.log('B+');
+    }
+    else if (number >= 75) {
+        console.log('B-');
+    }
+    else if (number >= 75) {
+        console.log('C+');
+    }
+    else if (number >= 75) {
+        console.log('C-');
+    }
+    else if (number >= 75) {
+        console.log('D+');
+    }
+    else if (number >= 75) {
+        console.log('D -');
+    }
+    else if (number < 33) {
+        console.log('F');
+    }
+    else
+        console.log('F');
+
+}
+
+var number = gradePoint(90);
+console.log(number);
+
+
--- all_basic.js
+++ all_basic.js
@@ -474,3 +474,188 @@ for (var i = 0; i < numbers.length; i++) {
 }
 
 
+// let, const  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// let price = 50;
+price = 30;
+price = 20;
+
+const Name = 'Ayatullah';
+
+
+// Inch to Feet, miles to kilometer using function >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+var Inches = 132;
+var feet = Inches / 12;
+// console.log('132 inche =', feet, 'feet');
+
+
+// Inch to Feet
+function inchToFeet(inch) {
+    var feet = inch / 12;
+    return feet;
+}
+
+var my1stInch = 1200;
+var result = inchToFeet(my1stInch);
+// console.log('1200 inche =', result, 'feet');
+
+var my2ndInch = 2400;
+var result = inchToFeet(my2ndInch);
+// console.log('2200 inche =', result, 'feet');
+
+var my3rdInch = 3600;
+var result = inchToFeet(my3rdInch);
+// console.log('3200 inche =', result, 'feet');
+
+
+// miles to kilometer
+function milesToKilometer(mile) {
+    var kilometers = mile * 1.609344;
+    return kilometers;
+}
+
+var my1stmiles = 10;
+var result = milesToKilometer(my1stmiles);
+// console.log('10 mile =', result, 'kilometers');
+
+var my2ndmiles = 20;
+var result = milesToKilometer(my2ndmiles);
+// console.log('20 mile =', result, 'kilometers');
+
+var my3rdmiles = 30;
+var result = milesToKilometer(my3rdmiles);
+// console.log('30 mile =', result, 'kilometers');
+
+
+// even and odd number using function  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// vag ses ber korar neom 
+// console.log(4 % 2);
+var number = 4;
+var vagses = 4 % 2;
+// console.log(vagses == 0);
+
+// Even with function
+function even(number) {
+    if (number % 2 == 0) {
+        return true;
+    }
+    return false;
+}
+var num1 = 50;
+var result = even(num1);
+// console.log('50 is even =', result);
+
+var num1 = 55;
+var result = even(num1);
+// console.log('55 is even =', result);
+
+
+// Odd with function
+function Odd(number) {
+    if (number % 2 != 0) {
+        return true;
+    }
+    return false;
+}
+var num1 = 45;
+var result = Odd(num1);
+// console.log('45 is odd =', result);
+
+var num1 = 124;
+var result = Odd(num1);
+// console.log('124 is odd =', result);
+
+
+
+// Check whether a year is a Leap Year or not  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+function leapYear(year) {
+    if (year % 4 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+
+}
+var son = 2024;
+var callResult = leapYear(son);
+// console.log('2024 is a leapyear =', callResult);
+
+var son = 2025;
+var callResult = leapYear(son);
+// console.log('2025 is a leapyear =', callResult);
+
+
+// program to check leap year (copy)
+function checkLeapYear(year) {
+
+    //three conditions to find out the leap year
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        // console.log(year + ' is a leap year');
+    } else {
+        // console.log(year + ' is not a leap year');
+    }
+}
+// take input
+// const year = prompt('Enter a year:');
+
+// checkLeapYear(year);
+checkLeapYear(2024);
+checkLeapYear(2025);
+checkLeapYear(2000);
+checkLeapYear(1800);
+checkLeapYear(1900);
+
+
+
+// Calculate Factorial of a number using for loop >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+var factorial = 1;
+for (var i = 1; i <= 7; i++) {
+    factorial = factorial * i;
+}
+// console.log(factorial);
+
+// Factorial with function 
+function getFactorial(number) {
+    var factorial = 1;
+    for (var i = 1; i <= number; i++) {
+        factorial = factorial * i;
+    }
+    return factorial;
+}
+
+var result = getFactorial(7);
+// console.log(result);
+
+// Factorial using a while loop or a decrementing loop
+// function getFactorial(number) {
+//     var factorial = 1;
+//     var i = 1;
+//     while (i <= number) {
+//         factorial = factorial * i;
+//         i++;
+//     }
+//     return factorial;
+// }
+
+// var result = getFactorial(7);
+// // console.log(result);
+
+function getFactorial2(number) {
+    var factorial = 1;
+    var i = number;
+    while (i >= 1) {
+        factorial = factorial * i;
+        i--;
+    }
+    return factorial;
+}
+
+var result = getFactorial2(6);
+// console.log(result);
+
+
+
+
+
+
+
